187ad366d34b529c38917d68ef18f8cd754d512d
James Goff <james_goff88@outlook.com>
2022-01-02 22:18:30 -0600

    global css

---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+
+
+export default function EventsPage() {
+    return (
+        <div>
+            <h1>My Events</h1>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout"
+
+export default function HomePage() {
+  return (
+
+    <Layout >
+
+      <h1>Home</h1>
+    </Layout>
+  )
+}
